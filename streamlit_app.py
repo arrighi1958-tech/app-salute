c548615e3cdad47b80cf84bfa44382b88658d9fd
arrighi1958-tech <arrighi1958@gmail.com>
2026-07-17 10:23:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,11 +146,22 @@ with tab_oggi:
         </div>
     """, unsafe_allow_html=True)
     
-    st.markdown(f"""
-        <div class="metric-card bg-verde">
-            <div class="metric-title">Ultimo Esito ECG Registrato</div>
-            <div class="metric-value">{prendi_dato(13, "ARITMIA")}</div>
-            <div class="metric-status">🟢 Registrato</div>
+       st.markdown(f"""
+        <div class="metric-card bg-giallo">
+            <div class="metric-title">Stato Regolarità Ritmo Circadiano</div>
+            <div class="metric-value">{prendi_dato(23, "Cattivo")}</div>
+            <div class="metric-status">🟡 Da Regolarizzare</div>
+        </div>
+    """, unsafe_allow_html=True)
+
+with tab_medie:
+    st.subheader("Analisi delle Medie Storiche")
+    st.info("In questa sezione verranno inseriti i calcoli aggregati sul lungo periodo.")
+
+with tab_trend:
+    st.subheader("Grafici e Trend temporali")
+    st.info("In questa sezione potrai visualizzare l'andamento grafico dei tuoi parametri medici.")
+
         </div>
     """, unsafe_allow_html=True)
     
